224214ec10a166fd3d025a598a32ace10db365d1
oladeleajayi <oladeleajayi37@gmail.com>
2024-11-19 09:01:34 +0100

    Add files via upload

---
+++ schedule.js
@@ -0,0 +1,30 @@
+
+// //import
+// const express = require('express');
+// const path = require('path');
+// require('dotenv').config();
+
+// const authRoutes = require('./routes/auth');
+
+// const app = express();
+
+// //set-up middleware
+// app.use(express.static(path.join(__dirname, '/')));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use('/auth', authRoutes)
+
+// app.get('/', (request, response) => {
+//     response.sendFile(path.join(__dirname, 'schedule.html'));
+// });
+
+// app.get('/schedule', (request, response) => {
+//     response.sendFile(path.join(__dirname, 'schedule.html'));
+// });
+
+// const PORT = process.env.PORT;
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running at http://127.0.0.1:${PORT}`)
+// });
